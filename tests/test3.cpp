0299013fc0ac52a5939146302f591e1ba526a6e2
justleon <maniak656@gmail.com>
2021-06-10 20:21:18 +0200

    More tests

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -33,6 +33,17 @@ TEST(OutputTest, Output) {
     t3.push("Tak");
     Linda::output(t3);
     ASSERT_EQ(find(p3, "tuplespace/s.linda", false).to_string(), t3.to_string());
+
+    Linda::Tuple t4;
+    Linda::Pattern p4;
+    p4.add(Linda::PatternEntryType::Equal, 12);
+    p4.add(Linda::PatternEntryType::GreaterOrEqual, 4.0f);
+    p4.add(Linda::PatternEntryType::Equal, "Tak");
+    t4.push(12);
+    t4.push(4.0f);
+    t4.push("Tak");
+    Linda::output(t4);
+    ASSERT_EQ(find(p4, "tuplespace/ifs.linda", false).to_string(), t4.to_string());
 }
 
 TEST(OutputTest, OutputTooBigTuple) {
@@ -54,36 +65,86 @@ TEST(OutputTest, OutputEmpty) {
     ASSERT_THROW(Linda::output(t), Linda::Exception::TupleSpaceException);
 }
 
-TEST(ReadTests, SimpleRead) {
+TEST(ReadTest, SimpleRead) {
     Linda::create();
     Linda::connect();
 
     Linda::Pattern p1;
-    p1.add<Linda::PatternEntryType::Equal>(12);
+    p1.add(Linda::PatternEntryType::Equal,12);
     ASSERT_EQ(Linda::read(p1).to_string(), "(i:12)");
 
     Linda::Pattern p2;
-    p2.add<Linda::PatternEntryType::GreaterOrEqual>(4.0f);
+    p2.add(Linda::PatternEntryType::GreaterOrEqual,4.0f);
     ASSERT_EQ(Linda::read(p2).to_string(), "(f:4.0)");
 
     Linda::Pattern p3;
-    p3.add<Linda::PatternEntryType::Equal>("Tak");
+    p3.add(Linda::PatternEntryType::Equal,"Tak");
     ASSERT_EQ(Linda::read(p3).to_string(), "(s:\"Tak\")");
+
+    Linda::Pattern p4;
+    p4.add(Linda::PatternEntryType::Equal, 12);
+    p4.add(Linda::PatternEntryType::GreaterOrEqual, 4.0f);
+    p4.add(Linda::PatternEntryType::Equal, "Tak");
+    ASSERT_EQ(Linda::read(p4).to_string(), "(i:12, f:4.0, s:\"Tak\")");
 }
 
-TEST(InputTests, SimpleInput) {
+TEST(ReadTest, ReadTooBigPattern) {
+    Linda::create();
+    Linda::connect();
+
+    Linda::Pattern p;
+    for(int i = 0; i < 8; i++)
+        p.add<Linda::PatternEntryType::Equal>("testestestestestestestestsetsetsetestestestestestesttestestestestestestest");
+
+    ASSERT_THROW(Linda::read(p), Linda::Exception::TupleSpaceException);
+}
+
+TEST(ReadTest, ReadEmpty) {
+    Linda::create();
+    Linda::connect();
+
+    Linda::Pattern p;
+    ASSERT_THROW(Linda::read(p), Linda::Exception::TupleSpaceException);
+}
+
+TEST(InputTest, SimpleInput) {
     Linda::create();
     Linda::connect();
 
     Linda::Pattern right;
-    right.add<Linda::PatternEntryType::Equal>(12);
+    right.add(Linda::PatternEntryType::Equal,12);
     ASSERT_EQ(Linda::input(right).to_string(), "(i:12)");
 
     Linda::Pattern p2;
-    p2.add<Linda::PatternEntryType::GreaterOrEqual>(4.0f);
+    p2.add(Linda::PatternEntryType::GreaterOrEqual,4.0f);
     ASSERT_EQ(Linda::input(p2).to_string(), "(f:4.0)");
 
     Linda::Pattern p3;
-    p3.add<Linda::PatternEntryType::Equal>("Tak");
+    p3.add(Linda::PatternEntryType::Equal,"Tak");
     ASSERT_EQ(Linda::input(p3).to_string(), "(s:\"Tak\")");
+
+    Linda::Pattern p4;
+    p4.add(Linda::PatternEntryType::Equal, 12);
+    p4.add(Linda::PatternEntryType::GreaterOrEqual, 4.0f);
+    p4.add(Linda::PatternEntryType::Equal, "Tak");
+    ASSERT_EQ(Linda::input(p4).to_string(), "(i:12, f:4.0, s:\"Tak\")");
+}
+
+TEST(InputTest, InputTooBigPattern) {
+    Linda::create();
+    Linda::connect();
+
+    Linda::Pattern p;
+    for(int i = 0; i < 8; i++)
+        p.add<Linda::PatternEntryType::Equal>("testestestestestestestestsetsetsetestestestestestesttestestestestestestest");
+
+    ASSERT_THROW(Linda::input(p), Linda::Exception::TupleSpaceException);
+}
+
+TEST(InputTest, InputEmpty) {
+    Linda::create();
+    Linda::connect();
+
+    Linda::Pattern p;
+    ASSERT_THROW(Linda::input(p), Linda::Exception::TupleSpaceException);
 }
